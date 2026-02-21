8ca0d9def7b72721e36c75db5560a22d0dbbd6e2
Rahul Nokwal <nokwalrahul09@gmail.com>
2026-02-21 23:12:43 +0530

    add features section

--- src/script.js
+++ src/script.js
@@ -1,14 +1,16 @@
 const openBtn = document.querySelector("[data-mobile-open]");
 const closeBtn = document.querySelector("[data-mobile-close]");
 const menuBar = document.querySelector("[data-mobile-menu]");
-
+const body = document.querySelector("body");
 const toggleMenu = (isOpen) => {
   if (isOpen) {
     menuBar.classList.remove("opacity-0", "pointer-events-none");
     menuBar.classList.add("opacity-100", "pointer-events-all");
+    body.style.overflow = "hidden";
   } else {
     menuBar.classList.remove("opacity-100", "pointer-events-all");
     menuBar.classList.add("opacity-0", "pointer-events-none");
+    body.style.overflow = "auto";
   }
 };
 
